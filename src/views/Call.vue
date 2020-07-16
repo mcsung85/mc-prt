88dd6de4e75fc41bd6a0464a9d26fd6de818a6fe
mcsung <mcsung@thewc.co.kr>
2020-07-16 18:58:18 +0900

    20200716

--- src/views/Call.vue
+++ src/views/Call.vue
@@ -8,94 +8,36 @@
     </nav>
     <div id="content">
       <!-- 실제 사용 될 마크업 -->
-      <div class="callCon">
-        <!-- 리스트 -->
-        <div class="box listCon">
-          <div class="listBox">
-            <div class="tab">
-              <ul>
-                <li><a>상담현황</a></li>
-                <li><a>예약현황</a></li>
-              </ul>
-            </div>
+      <div class="callCon" :class="{ritPop: !changLayer}">
+        <!-- callList -->
+        <div class="callList">
+          <div>{{changLayer}}Call 상담 대기중 <span>(8)</span></div>
+          <ul>
+            <li>010-1234-1234</li>
+            <li>010-1234-1234</li>
+            <li>010-1234-1234</li>
+          </ul>
+        </div>
+        <!-- callList -->
 
-            <div class="list">
-              <div>
-                <a>
-                  <p>8 <span>1</span></p>
-                  <p>대기중</p>
-                </a>
-                <a>
-                  <p>8 <span>1</span></p>
-                  <p>상담완료</p>
-                </a>
-                <a>
-                  <p>8 <span>1</span></p>
-                  <p>상담이관</p>
-                </a>
-                <a>
-                  <p>8 <span>1</span></p>
-                  <p>전체</p>
-                </a>
-              </div>
-              <div>
-                검색
-              </div>
-
-              <div>
-                <div>
-                  <ul>
-                    <li>
-                      <div class="">
-                        <dl>
-                          <dt>김덕배 고갱 <span>수신통화</span></dt>
-                          <dd>010-1234-1234</dd>
-                          <dd>오후 4:44</dd>
-                          <dd>11:11:11</dd>
-                        </dl>
-                      </div>
-                      <div class="">
-                        <p>대기중</p>
-                        <span>서지혜</span>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-
-              </div>
-            </div>
-            <div class="list" style="display:none;">
-              예약현황 컨텐츠
-            </div>
-          </div>
-
-          <div class="listBtm">
-            <div class="tab">
-              <ul>
-                <li><a>상담 모니터링</a></li>
-                <li><a>다른 상담원 상태</a></li>
-              </ul>
-            </div>
-            <div class="list">
-              <ul>
-                <li>
-                  <dl>
-                    <dt>총 통화</dt>
-                    <dd>00:00:00</dd>
-                  </dl>
-                </li>
-              </ul>
-            </div>
-            <div class="list" style="display:none;">
-              다른 상담원 상태 컨텐츠
-            </div>
+        <!-- Left -->
+        <div class="leftCon scrollBox">
+          <div class="box">
+            LEFT
           </div>
         </div>
-        <!-- //리스트 -->
-        <div class="box">box2</div>
+        <!-- //Left -->
+
+        <!-- Center -->
+        <div class="cenCon scrollBox">
+          <div class="box">
+            Center
+          </div>
+        </div>
+        <!-- //Center -->
 
         <!-- Right -->
-        <div class="box ritCon">
+        <div class="ritCon scrollBox">
           <div class="ritNav">
             <ul>
               <li>icon</li>
@@ -107,146 +49,12 @@
           </div>
           <!-- 고객정보 -->
           <div class="ritBox on">
-            <div class="ritTit">
-              고객정보
-              <div class="close">
-                <a>X</a>
-              </div>
-            </div>
-            <!-- 일반정보 -->
-            <div class="ritSubBox">
-              <div class="subTit">일반정보</div>
-              <div class="ritType0">
-                <table>
-                  <colgroup>
-                    <col style="" />
-                    <col style="" />
-                  </colgroup>
-                  <tbody>
-                    <tr>
-                      <th>이름</th>
-                      <td>
-                        <input type="text" />
-                      </td>
-                    </tr>
-                    <tr>
-                      <th>전화번호</th>
-                      <td>
-                        <input type="text" />
-                      </td>
-                    </tr>
-                    <tr>
-                      <th>이메일</th>
-                      <td>
-                        <input type="text" />
-                      </td>
-                    </tr>
-                    <tr>
-                      <th>클레임정보</th>
-                      <td>
-                        <input type="text" />
-                      </td>
-                    </tr>
-                    <tr>
-                      <th>생년월일</th>
-                      <td>
-                        <input type="text" />
-                      </td>
-                    </tr>
-                    <tr>
-                      <th>성별</th>
-                      <td>
-                        <input type="text" />
-                      </td>
-                    </tr>
-                    <tr>
-                      <td colspan="2">
-                        <textarea></textarea>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-                <div class="ritBtn">
-                  <button>저장</button>
-                </div>
-              </div>
-            </div>
-            <!-- // 일반정보 -->
-
-            <!-- 상담내역 -->
-            <div class="ritSubBox">
-              <div class="subTit">상담이력</div>
-              <div class="ritType1">
-                <div class="subTopTit">상담내용</div>
-                <ul>
-                  <li>
-                    <dl>
-                      <dt>배송문의 > 배송지연 > 취소요청</dt>
-                      <dd class="text">ㅁ 주문번호 20200001- 000001 - 고객명 : 홍길동 - 연락처 : 010-1234-1234 - 상담내용 : 주문한지 10일 지났으나 배송지연으로 취소요청</dd>
-                      <dd class="date">2020-06-13 17:46:22</dd>
-                    </dl>
-                  </li>
-                  <li>
-                    <dl>
-                      <dt>배송문의 > 배송지연 > 취소요청</dt>
-                      <dd class="text">ㅁ 주문번호 20200001- 000001 - 고객명 : 홍길동 - 연락처 : 010-1234-1234 - 상담내용 : 주문한지 10일 지났으나 배송지연으로 취소요청</dd>
-                      <dd class="date">2020-06-13 17:46:22</dd>
-                    </dl>
-                  </li>
-                  <li>
-                    <dl>
-                      <dt>배송문의 > 배송지연 > 취소요청</dt>
-                      <dd class="text">ㅁ 주문번호 20200001- 000001 - 고객명 : 홍길동 - 연락처 : 010-1234-1234 - 상담내용 : 주문한지 10일 지났으나 배송지연으로 취소요청</dd>
-                      <dd class="date">2020-06-13 17:46:22</dd>
-                    </dl>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <!-- // 상담내역 -->
-
-            <!-- 관리메모 -->
-            <div class="ritSubBox">
-              <div class="subTit">관리메모</div>
-              <div class="ritType2">
-                <div class="subTopTit">상담내용</div>
-                <ul>
-                  <li>
-                    <dl>
-                      <dt>클레임 정도 상 -> 하로 하향 단, 세심한 관리 필요</dt>
-                      <dd>2020-06-13 137:46:22</dd>
-                      <dd>작성자 <span>서재 상담사</span></dd>
-                    </dl>
-                  </li>
-                  <li>
-                    <dl>
-                      <dt>클레임 정도 상 -> 하로 하향 단, 세심한 관리 필요</dt>
-                      <dd>2020-06-13 137:46:22</dd>
-                      <dd>작성자 <span>서재 상담사</span></dd>
-                    </dl>
-                  </li>
-                  <li>
-                    <dl>
-                      <dt>클레임 정도 상 -> 하로 하향 단, 세심한 관리 필요</dt>
-                      <dd>2020-06-13 137:46:22</dd>
-                      <dd>작성자 <span>서재 상담사</span></dd>
-                    </dl>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <!-- // 관리메모 -->
+            RIGHT
           </div>
-          <!-- // 고객정보 -->
-
-          <!-- 주문정보 -->
-          <div class="ritBox">
-            <div class="ritTit">주문정보</div>
-          </div>
-          <!-- // 주문정보 -->
 
         </div>
         <!-- //Right -->
+
       </div>
       <!-- // 실제 사용 될 마크업 -->
     </div>
@@ -258,7 +66,14 @@ export default {
   name: 'Call',
   data () {
     return {
+      changLayer: false
     }
+  },
+  created () {
+    this.changLayer = document.body.clientWidth > 1280
+    window.addEventListener('resize', (event) => {
+      this.changLayer = document.body.clientWidth > 1280
+    })
   }
 }
 </script>
@@ -276,8 +91,7 @@ body {
     left:0;
     width: 100%;
     height:80px;
-    border: 1px solid #000;
-    background-color: #000;
+    border-bottom: 1px solid #E5E8F2;
   }
   #nav {
     position: absolute;
@@ -286,27 +100,51 @@ body {
     padding-top: 80px;
     width: 100px;
     height: 100%;
-    border: 1px solid #000;
   }
   #content {
     position: relative;
     padding: 80px 0 0 100px;
     height: 100%;
-    border: 1px solid #000;
   }
   .callCon {
+    position: relative;
     display:table;
     width: 100%;
     height: 100%;
-    .box {
-      width: 33.33%;
-      float:left;
-      border: 1pxs solid #000;
+    .callList {
+      position:absolute;
+      top: 0;
+      left: 306px;
+      border-bottom: 1px solid #E5E8F2;
+      width: 100%;
+      height: 65px;
+      overflow:hidden;
+    }
+    .leftCon {
+      width: 306px;
+      border-right: 1px solid #E5E8F2;
+    }
+    .cenCon {
+      position:absolute;
+      top: 0;
+      left: 0;
+      padding:65px 453px 0 306px;
+      width: 100%;
+      height: 100%;
+      text-align: center;
+      .box {
+        height: 100%;
+        border-right: 1px solid #E5E8F2;
+      }
     }
     .ritCon {
-      position: relative;
+      position: absolute;
+      top:65px;
+      right:0;
       padding-right: 65px;
+      width:453px;
       height: 100%;
+      background: #fff;
       .ritNav {
         position: absolute;
         top:0;
@@ -358,6 +196,20 @@ body {
           border-top:1px solid #C4CDD5;
           border-bottom:1px solid #C4CDD5;
           background-color: #F0F0FA;
+        }
+      }
+    }
+    &.ritPop {
+      .cenCon {
+        padding-right: 0;
+      }
+      @media screen and (max-width: 1280px) {
+        .cenCon {
+          width: 100%;
+          min-width: 826px;
+          text-align:left;
+          border-bottom:1px solid #C4CDD5;
+          // background-color:red;
         }
       }
     }
